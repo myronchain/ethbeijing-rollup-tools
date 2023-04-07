5e87e10c109ff22da03d4b7e8f9ad4876421fa92
eric-g1g2 <eric@g1g2.xyz>
2023-04-07 12:00:51 +0800

    add tx job when run a rollup

--- server/server/rollup_mamager.go
+++ server/server/rollup_mamager.go
@@ -2,15 +2,19 @@ package server
 
 import (
 	"context"
+	"fmt"
+	"time"
 
 	"github.com/g1g2-lab/automation/l2"
 	"github.com/g1g2-lab/automation/pkg/db"
 	"github.com/g1g2-lab/automation/types"
+	"github.com/g1g2-lab/automation/util"
 )
 
 type Manager struct {
-	db  *db.LocalFileDatabase
-	cfg *l2.L2Config
+	db   *db.LocalFileDatabase
+	cfg  *l2.L2Config
+	jobs map[string]chan bool
 }
 
 func NewRollupManager(db *db.LocalFileDatabase,
@@ -18,6 +22,7 @@ func NewRollupManager(db *db.LocalFileDatabase,
 	return &Manager{
 		db,
 		cfg,
+		map[string]chan bool{},
 	}
 }
 
@@ -34,12 +39,53 @@ func (m *Manager) CreateRollup(req *types.CreateRollupRequest) error {
 		return err
 	}
 	err = l2.RunRollup(m.cfg, rollup, m.db)
+	if err != nil {
+		return err
+	}
+	err = m.runTxJobs(rollup)
 	return err
 }
 
 func (m *Manager) DeleteRollup(
 	name string,
 ) error {
+	m.stopTxJobs(name)
 	err := l2.StopRollupByName(context.Background(), m.cfg, name, m.Db())
 	return err
+}
+
+func (m *Manager) runTxJobs(rollup *types.Rollup) error {
+	quit, ok := m.jobs[rollup.Name]
+	if ok {
+		// already have a channel. stop it
+		quit <- true
+	}
+	pk := m.cfg.G1G2Admin.L1AdminPK
+	address, err := util.PrivateKeyToAddress(pk)
+	if err != nil {
+		return err
+	}
+	quit = make(chan bool)
+	go func() {
+		for {
+			select {
+			case <-quit:
+				return
+			default:
+				time.Sleep(time.Second * 2)
+				util.ExecWrapper(
+					fmt.Sprintf("cast send %s --value=10 --private-key=%s --rpc-url=%s --legacy", address.String(), pk, rollup.RpcUrl),
+				)
+			}
+		}
+	}()
+	m.jobs[rollup.Name] = quit
+	return nil
+}
+
+func (m *Manager) stopTxJobs(name string) {
+	quit, ok := m.jobs[name]
+	if ok {
+		quit <- true
+	}
 }
